6b9a2e3fd3d46965877667f297d7232658520bfa
Håvard Eilertsen <havardei@gmail.com>
2021-02-19 13:33:08 +0100

    adjusted template

--- main.tf
+++ main.tf
@@ -5,13 +5,10 @@ module "openstack" {
   public_key_path = ""
   image = ""
   flavor = ""
-  network_name = "test"
   external_net = ""
   floatingip_pool = ""
   number_of_instances = 1
-  instance_root_volume_size_in_gb = 80
-  instance_allowed_remote_ips = [] # ssh-access from
-  instance_allowed_ports = [] 
+  instance_allowed_remote_ips = [] #ssh-access
 }
 output "instance_ips" {
   value = "${module.openstack.instance_fips}"
